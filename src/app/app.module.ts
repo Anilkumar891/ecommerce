9fb55441074b46d6c7ab39423da2ccac378d20c1
Anilkumar891 <dubaanilkumar99@gmail.com>
2024-03-14 15:07:41 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material/material.module';
+import { PagesModule } from './pages/pages.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FotterComponent } from './components/fotter/fotter.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { firebaseConfig } from './environments/environments';
+import { PrasadDirective } from './prasad.directive';
+
+
+@NgModule({
+  declarations: [
+   AppComponent,
+   HeaderComponent,
+   FotterComponent,
+   PrasadDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    PagesModule,
+    HttpClientModule,
+    
+    AngularFireAuthModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/environments/environments.ts
@@ -0,0 +1,13 @@
+export const API_URL='https://dummyjson.com/products';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAljqytg-yjUPSuE0YgbEokF3TCykzxHvc",
+  authDomain: "main-project-a8ebd.firebaseapp.com",
+  projectId: "main-project-a8ebd",
+  storageBucket: "main-project-a8ebd.appspot.com",
+  messagingSenderId: "319740341650",
+  appId: "1:319740341650:web:69e18253bdfc2512d7254b",
+  databaseURL:'https://main-project-a8ebd-default-rtdb.firebaseio.com'
+  
+};
+
